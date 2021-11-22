08ea062ae1c0a97b52efd87c496045ab6bb8fcc9
root <root@localhost>
2021-11-22 13:13:04 +0600

    auto publish 1637565183930

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import random
+a=random.randint(1,10)
+b=int(input("enter a number betweeen 1-10:\n"))
+if a==b:
+  print("congrts,you won!")
+else:
+  print("comp chose ",a,"\nyou lost")
+
+
+
+
+
+
+
+
+
+
+
