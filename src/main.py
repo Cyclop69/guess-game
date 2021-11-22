1d5ca3ebca8a4e2c827f4594da200c50b49ed0f2
root <root@localhost>
2021-11-22 13:26:56 +0600

    auto publish 1637566016119

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 import random
-a=random.randint(1,10)
-b=int(input("enter a number betweeen 1-10:\n"))
-if a==b:
-  print("congrts,you won!")
-else:
-  print("comp chose ",a,"\nyou lost")
+s=0
+while True:
+  
+  a=random.randint(1,10)
+  b=input("\n\n\nenter a number betweeen 1-10:(if you want to quit type:q):\n")
+  if b=="q":
+    break
+  elif a==int(b):
+    print("congrts,you won!")
+    s=s+10
+    print("your new score: ",s)
+  else:
+    print("comp chose ",a,"\nyou lost")
 
 
 
